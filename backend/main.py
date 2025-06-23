b4ed2a4fdddbde6091d2fbcea7b3dc9492c15c74
divine <guptadivine0611@gmail.com>
2025-06-23 14:02:47 +0530

    FastAPI Server

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,17 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from bson.json_util import dumps
 from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+import os
 import json
+
+# Load environment variables from .env
+load_dotenv()
 
 app = FastAPI()
 
-# MongoDB URI
-MONGO_URI = "mongodb+srv://clinisight:WySGSgkW8m0JvhCw@cluster0.p6w0nsx.mongodb.net/?retryWrites=true&w=majority"
+# Get MongoDB URI from .env
+MONGO_URI = os.getenv("MONGO_URI")
 
 # Connect to MongoDB
 client = MongoClient(MONGO_URI)
@@ -24,7 +29,6 @@ def read_root():
     patients = list(patients_collection.find())
     records = list(records_collection.find())
 
-    # Use JSONResponse to return the result properly formatted
     return JSONResponse(content={
         "doctors": json.loads(dumps(doctors)),
         "patients": json.loads(dumps(patients)),
